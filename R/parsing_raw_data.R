976debddea9907df0a4c68b8961b6629205f97e1
Alejandra H. Segura <43819685+AleSR13@users.noreply.github.com>
2021-02-08 18:21:09 +0100

    First version of the refsamp package.

---
+++ R/parsing_raw_data.R
@@ -0,0 +1,183 @@
+### Functions to get run info -------------------------------------------------
+
+#' Get run date from iRODS collection name of Juno output.
+#'
+#' @param input_dir Collection name of Juno output as assigned by iRODS in
+#'   previous pipeline. It MUST start with the date in the format "yymmdd".
+#'
+#' @return String with date-like format (yy-mm-dd).
+#' @export
+#'
+#' @importFrom dplyr %>%
+#'
+#' @examples get_run_date("juno_results/200922_NB502001_0153_AHGNLNAFX2_0007")
+get_run_date <- function(input_dir){
+  stopifnot( stringr::str_detect(input_dir, "\\d{6}") )
+
+  if ( stringr::str_detect(input_dir, "/") ){
+    input_dir <- basename(input_dir)
+  }
+
+  input_dir %>%
+    stringr::str_extract("\\d{6}") %>%
+    purrr::map_chr(stringr::str_replace, "(\\d{2})(\\d{2})(\\d{2})$","\\1-\\2-\\3")
+
+}
+
+### Functions to get and parse tool reports -----------------------------------
+
+#' Find tool reports in Juno results.
+#'
+#' @param input_dir Path to directory with results from Juno pipeline.
+#' @param tool c("quast", "bbtools", "checkm").
+#'
+#' @return The file path(s) of the desired report (tool) type in the input
+#'   directory.
+#'
+#' @importFrom dplyr %>%
+#'
+find_tool_reports <- function(input_dir=character(),
+                              tool=c("quast", "bbtools", "checkm")){
+
+  stopifnot( dir.exists(input_dir) )
+
+  pattern_name <- "_report.tsv"
+
+  if (tool == "quast"){
+    pattern_path <- "quast|QUAST"
+  } else if (tool == "checkm"){
+    pattern_path <- "checkm|CheckM|CHECKM"
+  } else if (tool == "bbtools"){
+    pattern_path <- "bbtools_combined"
+  } else {
+    stop("argument tool must be either 'quast', 'bbtools' or 'checkm'")
+  }
+
+  list.files( input_dir,  pattern = pattern_name, full.names = T, recursive = T) %>%
+    `[`(stringr::str_detect(., pattern_path ))
+}
+
+
+
+#' Read tsv report
+#'
+#' @param file_path Path to tsv report.
+#'
+#' @return Tibble containing the tsv report
+#'
+#' @importFrom dplyr %>%
+#'
+#'
+read_tsv_report <- function(file_path){
+  stopifnot(file.exists(file_path))
+
+  if( length(file_path) > 1 ){
+    warning("The input directory contains more than one report.tsv.
+            The different report.tsv files will be merged in one
+            single dataframe.")
+    report <- suppressWarnings(suppressMessages(
+      purrr::map(file_path, readr::read_tsv, col_names = TRUE) )) %>%
+      dplyr::bind_rows()
+  } else{
+    report <- suppressWarnings( suppressMessages(readr::read_tsv(file_path, col_names = TRUE) ))
+  }
+
+  return(report)
+
+}
+
+
+#' Read information from QUAST report
+#'
+#' @param input_dir Path to directory with results from Juno pipeline.
+#'
+#' @return Tidy data frame containing information from QUAST report to be used
+#'   for this QC report
+#' @export
+#'
+#' @importFrom dplyr %>%
+#'
+extract_quast <- function(input_dir = character()){
+  file_path <- find_tool_reports(input_dir, "quast")
+  read_tsv_report(file_path) %>%
+    dplyr::select("Assembly", "# contigs", "Total length", "GC (%)", "N50", "L50") %>%
+    dplyr::rename("Sample" = "Assembly")
+}
+
+#' Read information from bbtools report
+#'
+#' @param input_dir Path to directory with results from Juno pipeline.
+#'
+#' @return Tidy data frame containing information from bbtools report to be used
+#'   for this QC report.
+#' @export
+#'
+#' @importFrom dplyr %>%
+#'
+extract_bbtools <- function(input_dir = character()){
+  file_path <- find_tool_reports(input_dir, "bbtools")
+  read_tsv_report(file_path) %>%
+    dplyr::select("Sample",	"Percent mapped",	"Average coverage",
+                  #"Reads", "Mapped reads", "Mapped bases", "Ref scaffolds", "Ref bases",	"Percent proper pairs",
+                  "Percent of reference bases covered")
+}
+
+#' Read information from CheckM report
+#'
+#' @param input_dir Path to directory with results from Juno pipeline.
+#'
+#' @return Tidy data frame containing information from CheckM report to be used
+#'   for this QC report
+#' @export
+#'
+#' @importFrom dplyr %>%
+#'
+extract_checkm <- function(input_dir = character()){
+  file_path <- find_tool_reports(input_dir, "checkm")
+  read_tsv_report(file_path) %>%
+    dplyr::rename( "Sample" = "sample" ) %>%
+    dplyr::select("Sample", "completeness", "contamination",	"strain_heterogeneity") %>%
+    dplyr::mutate("Sample" = stringr::str_remove(Sample, "L$"))
+}
+
+
+
+### Functions to merge datasets -----------------------------------------------
+
+#' Merge datasets by sample
+#'
+#' @param vector_w_dataframes Character vector with the name of the datasets to
+#'   merge. Each dataset MUST contain at least one column called "Sample" that
+#'   is common between them.
+#'
+#' @param run_date Character vector with the date in the format "yy-mm-dd" as
+#'   given by refsamp::get_run_date()
+#'
+#' @return Tibble with the different datasets provided in vector_w_dataframes
+#'   bound by the 'Sample' column.
+#' @export
+#'
+merging_by_sample <- function(vector_w_dataframes, run_date){
+  stopifnot(is.character(vector_w_dataframes))
+  stopifnot(is.character(run_date))
+
+  # Read datasets and make sure they have a "Sample" column
+  datasets_to_merge <- vector_w_dataframes %>%
+    purrr::map(as.name) %>%
+    purrr::map(eval)
+
+  # Merge datasets by Sample
+  merged_dataset <- datasets_to_merge[[1]]
+  for(i in 2:length(datasets_to_merge)){
+    merged_dataset <- dplyr::full_join(merged_dataset, datasets_to_merge[[i]], by = "Sample")
+  }
+
+  # Add genus name and run_date
+  merged_dataset <- merged_dataset %>%
+    left_join(refsamp::refsamp_genus, by = "Sample")  %>%
+    mutate("Run_date" = run_date)
+
+  return(merged_dataset)
+
+}
+
---
+++ R/plots.R
@@ -0,0 +1,64 @@
+### Plots QUAST ---------------------------------------------------------------
+
+#' Plot metric vs time
+#'
+#' @param dataset Data frame to use for plot. It must contain at least one
+#'   character column named "Run_date" (yy-mm-dd) and one character column named
+#'   "Sample".
+#' @param metric Character vector length 1 with the name of the metric to plot
+#'   (metric should be one of the column names of dataset)
+#'
+#' @return ggplot object containing the plot to be added
+#' @export
+#'
+#' @import ggplot2
+#'
+plot_time_metrics <- function(dataset, metric){
+  stopifnot("Sample" %in% colnames(dataset))
+  stopifnot("Run_date" %in% colnames(dataset))
+  stopifnot(metric %in% colnames(dataset))
+
+  dataset$Run_date <- as.Date(dataset$Run_date)
+
+  ggplot( dataset, aes(x = Run_date, y = !!as.name(metric), color = Genus))+
+    geom_smooth(formula = y ~ x, show.legend = F, method = "loess")+
+    geom_point(size = 2)+
+    scale_color_brewer(palette = "Dark2", na.value = "gray")+
+    theme_light()+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+}
+
+
+#' Make reference samples report
+#'
+#' @param input_dir Path to directory with results from Juno pipeline.
+#'   Collection name of Juno output as assigned by iRODS in previous pipeline.
+#'   It MUST start with the date in the format "yymmdd"
+#' @param output_report Desired file name of the output report (.html
+#'   extension).
+#' @param history_data Path to history_data. If not provided, a history_data.csv
+#'   file will be created in the same directory where the output_report is.
+#'
+#' @return Html report (file name taken from output_report parameter) and a csv
+#'   file with the history data.
+#' @export
+#'
+make_refsamp_report <- function(input_dir, output_report, history_data = NULL){
+  stopifnot(dir.exists(input_dir))
+  stopifnot(dir.exists(dirname(output_report)))
+  stopifnot(stringr::str_detect(output_report, ".html$"))
+
+  if( is.null(history_data) ){
+    history_data <- paste0( dirname(output_report), "history_data.csv" )
+  } else{
+    stopifnot(dir.exists(dirname(history_data)))
+    stopifnot(stringr::str_detect(history_data, ".csv$"))
+  }
+
+  rmarkdown::render(system.file("rmd", "Report_RefSamples.Rmd", package = "refsamp"),
+                   output_file = output_report,
+                   envir = parent.frame(), # See explanation: https://github.com/rstudio/rmarkdown/issues/934
+                   params = list(input_dir = input_dir,
+                                 history_data = history_data))
+}
+
